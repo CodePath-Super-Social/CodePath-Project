5a7898ae4649c2d1867a54707bebfb1cb3cfcb74
Tony Liu <tliu5175@gmail.com>
2022-02-25 01:44:50 -0800

    finished twitter login

--- app/src/main/java/com/supersocial/fragments/SettingsFragment.kt
+++ app/src/main/java/com/supersocial/fragments/SettingsFragment.kt
@@ -1,18 +1,36 @@
 package com.supersocial.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.supersocial.R
+import com.supersocial.SettingsActivity
+
+
+private const val TAG = "Settings Segment"
 
 class SettingsFragment : Fragment() {
+
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btnTwitterLogin).setOnClickListener {
+            val intent = Intent(activity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
